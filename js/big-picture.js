ba5c3af49748942705227fc046b468c1102eb82b
Kseniia <inanast153@gmail.com>
2023-12-26 21:26:09 +0100

    Přidá efekty, změní obrazky, přidá slider na portfolio

--- js/big-picture.js
+++ js/big-picture.js
@@ -2,34 +2,64 @@ const bigPictureContainer = document.querySelector('.big-picture');
 const bigPictureClose = bigPictureContainer.querySelector('.big-picture__cancel');
 const bigPictureImage = bigPictureContainer.querySelector('.big-picture__img img');
 const fullscreenTriggers = document.querySelectorAll('.js-fullscreen-trigger');
+const prevButton = document.querySelector('.big-picture__slider-button--prev');
+const nextButton = document.querySelector('.big-picture__slider-button--next');
+
+const images = Array.from(fullscreenTriggers).map(trigger => trigger.src);
+let currentIndex = 0;
 
 const isEscapeKey = (evt) => evt.key === 'Escape';
+const isLeftArrowKey = (evt) => evt.key === 'ArrowLeft';
+const isRightArrowKey = (evt) => evt.key === 'ArrowRight';
 
-fullscreenTriggers.forEach(trigger => {
-  trigger.addEventListener('click', () => {
-    const src = trigger.getAttribute('src');
-    openBigPicture(src);
-  });
-});
+const updateButtonsState = () => {
+  prevButton.disabled = currentIndex === 0;
+  nextButton.disabled = currentIndex === images.length - 1;
+};
 
-function openBigPicture(src) {
-  bigPictureImage.src = src;
+const openBigPicture = (index) => {
+  bigPictureImage.src = images[index];
   bigPictureContainer.classList.remove('visually-hidden');
   document.body.classList.add('modal-open');
   document.addEventListener('keydown', onDocumentKeydown);
-}
+  updateButtonsState();
+};
 
-function closeBigPicture() {
+const closeBigPicture = () => {
   bigPictureContainer.classList.add('visually-hidden');
   document.body.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
-}
+};
 
-function onDocumentKeydown(evt) {
+const navigateSlider = (direction) => {
+  if (direction === 'prev' && currentIndex > 0) {
+    openBigPicture(--currentIndex);
+  } else if (direction === 'next' && currentIndex < images.length - 1) {
+    openBigPicture(++currentIndex);
+  }
+};
+
+const onDocumentKeydown = (evt) => {
   if (isEscapeKey(evt)) {
     evt.preventDefault();
     closeBigPicture();
+  } else if (isLeftArrowKey(evt)) {
+    navigateSlider('prev');
+  } else if (isRightArrowKey(evt)) {
+    navigateSlider('next');
   }
-}
+};
+
+fullscreenTriggers.forEach((trigger, index) => {
+  trigger.addEventListener('click', () => {
+    currentIndex = index;
+    openBigPicture(currentIndex);
+  });
+});
+
+prevButton.addEventListener('click', () => navigateSlider('prev'));
+nextButton.addEventListener('click', () => navigateSlider('next'));
 
 bigPictureClose.addEventListener('click', closeBigPicture);
+
+updateButtonsState();
